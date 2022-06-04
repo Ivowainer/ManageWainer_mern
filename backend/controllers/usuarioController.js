c9d57921fd1b79231a3b4d5a81a022964003544a
Ivowainer <ivancamposwainer@gmail.com>
2022-06-04 15:18:44 -0300

    Model interacting with the BD

--- backend/controllers/usuarioController.js
+++ backend/controllers/usuarioController.js
@@ -1,8 +1,14 @@
-import Usuario from "../models/Usuario"
+import Usuario from "../models/Usuario.js"
 
-export const registrar = (req, res) => {
+export const registrar = async (req, res) => {
+    try {
+        const usuario = new Usuario(req.body)
 
-    console.log(req.body)
-    
-    res.json('D')
+        const usuarioAlmacenado = await usuario.save()
+
+        res.json(usuarioAlmacenado)
+
+    } catch (error) {
+        console.log(error)
+    }
 }
