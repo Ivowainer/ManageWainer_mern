133782ade72d432cd11f9f6102593d68f11f5bc4
Ivowainer <ivancamposwainer@gmail.com>
2022-06-09 17:42:38 -0300

    Authtentication, validation and JsonWebToken

--- backend/controllers/usuarioController.js
+++ backend/controllers/usuarioController.js
@@ -1,8 +1,11 @@
 import Usuario from "../models/Usuario.js"
+import generarId from "../helpers/generarId.js"
+import generarJWT from "../helpers/generarJWT.js"
 
 export const registrar = async (req, res) => {
-    //Evitar registros duplicados
     const { email } = req.body
+
+    //Evitar registros duplicados
     const existeUsuario = await Usuario.findOne({ email })
 
     if(existeUsuario) {
@@ -10,8 +13,10 @@ export const registrar = async (req, res) => {
         return res.status(400).json({ msg: error.message })
     }
 
+    //Guarda usuario 
     try {
         const usuario = new Usuario(req.body)
+        usuario.token = generarId()
 
         const usuarioAlmacenado = await usuario.save()
 
@@ -20,4 +25,39 @@ export const registrar = async (req, res) => {
     } catch (error) {
         console.log(error)
     }
+}
+
+export const autenticar = async (req, res) => {
+    const { email, password } = req.body
+
+    //Comprobar si el usuario existe
+    const usuario = await Usuario.findOne({ email })
+
+    if(!usuario) {
+        const error = new Error('El usuario no existe');
+        return res.status(404).json({ msg: error.message })
+    }
+
+    //Comprobar si el usuario está confirmado
+    if(!usuario.confirmado) {
+        const error = new Error('Tu cuenta no ha sido confirmada');
+        return res.status(403).json({ msg: error.message })
+    }
+
+    //Comprobar password
+    if(await usuario.comprobarPassword(password)){
+        console.log('es correcto')
+    } else {
+        const error = new Error('La contraseña es incorrecta')
+        return res.status(403).json({ 
+            _id: usuario._id,
+            nombre: usuario.nombre,
+            email: usuario.email,
+            token: generarJWT(usuario._id)
+         })
+    }
+}
+
+export const confirmar = async (req, res) => {
+    res.send(req.params.token)
 }
--- backend/routes/usuariosRoutes.js
+++ backend/routes/usuariosRoutes.js
@@ -1,8 +1,12 @@
 import express from 'express'
-import { registrar } from '../controllers/usuarioController.js';
+import { registrar, autenticar, confirmar } from '../controllers/usuarioController.js';
 const router = express.Router();
 
 // Autenticación, registro y confirmación de usuarios
-router.post('/', registrar) //Crea un nuevo user
+router.post('/', registrar) 
 
-export default router //Lo exporta
+router.post('/login', autenticar)
+
+router.get('/confirmar/:token', confirmar)
+
+export default router
