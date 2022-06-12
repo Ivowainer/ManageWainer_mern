8e3b753716b2c6fd875d51a1ad2908ee22ff3741
Ivowainer <ivancamposwainer@gmail.com>
2022-06-12 20:00:39 -0300

    Creating Project

--- backend/controllers/proyectoController.js
+++ backend/controllers/proyectoController.js
@@ -1,8 +1,21 @@
+import Proyecto from "../models/Proyecto.js"
+
 export const obtenerProyectos = async (req, res) => {
     res.send('s')
 }
 
-export const nuevoProyecto = async (req, res) => {}
+export const nuevoProyecto = async (req, res) => {
+    const proyecto = new Proyecto(req.body)
+    proyecto.creador = req.usuario._id
+
+    try {
+        const proyectoAlmacenado = await proyecto.save()
+
+        res.json(proyectoAlmacenado)
+    } catch (error) {
+        console.log(error)
+    }
+}
 
 export const obtenerPoryecto = async (req, res) => {}
 
