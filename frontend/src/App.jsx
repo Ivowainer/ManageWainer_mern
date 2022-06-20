e55dbdc6ba279d44963448c4319ab220f39ffc46
Ivowainer <ivancamposwainer@gmail.com>
2022-06-20 10:14:59 -0300

    Project Provider

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,9 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Proyectos from './pages/Proyectos'
 import RutaProtegida from './layouts/RutaProtegida'
-import { AuthProvider } from './context/AuthProvider'
+import AuthLayout from './layouts/AuthLayout'
 
 // Pages
-import AuthLayout from './layouts/AuthLayout'
 import ConfirmarCuenta from './pages/ConfirmarCuenta'
 import Login from './pages/Login'
 import NuevoPassword from './pages/NuevoPassword'
@@ -12,24 +11,29 @@ import OlvidePassword from './pages/OlvidePassword'
 import Registrar from './pages/Registrar'
 import NuevoProyecto from './pages/NuevoProyecto'
 
+import { AuthProvider } from './context/AuthProvider'
+import { ProyectosProvider } from './context/ProyectosProvider'
+
 function App() {
   return (
     <BrowserRouter>
       <AuthProvider>
-        <Routes>
-          <Route path='/' element={<AuthLayout />} >
-            <Route index element={<Login />} />
-            <Route path='registrar' element={<Registrar />} />
-            <Route path='olvide-password' element={<OlvidePassword />} />
-            <Route path='olvide-password/:token' element={<NuevoPassword />} />
-            <Route path='confirmar/:id' element={<ConfirmarCuenta />} />
-          </Route>
+        <ProyectosProvider>
+          <Routes>
+            <Route path='/' element={<AuthLayout />} >
+              <Route index element={<Login />} />
+              <Route path='registrar' element={<Registrar />} />
+              <Route path='olvide-password' element={<OlvidePassword />} />
+              <Route path='olvide-password/:token' element={<NuevoPassword />} />
+              <Route path='confirmar/:id' element={<ConfirmarCuenta />} />
+            </Route>
 
-          <Route path="/proyectos" element={<RutaProtegida />}>
-            <Route index element={<Proyectos />} />
-            <Route path="crear-proyecto" element={<NuevoProyecto />} />
-          </Route>
-        </Routes>
+            <Route path="/proyectos" element={<RutaProtegida />}>
+              <Route index element={<Proyectos />} />
+              <Route path="crear-proyecto" element={<NuevoProyecto />} />
+            </Route>
+          </Routes>
+        </ProyectosProvider>
       </AuthProvider>
     </BrowserRouter>
   )
