cb455c55a2ba3985d370276be7faf9f4ef038fb8
Ivowainer <ivancamposwainer@gmail.com>
2022-06-20 11:20:14 -0300

    Project create

--- frontend/src/components/FormularioProyecto.jsx
+++ frontend/src/components/FormularioProyecto.jsx
@@ -1,13 +1,39 @@
 import { useState } from 'react'
+import useProyectos from '../hooks/useProyectos'
+import Alerta from './Alerta'
 
 const FormularioProyecto = () => {
   const [nombre, setNombre] = useState('')
   const [descripcion, setDescripcion] = useState('')
   const [fechaEntrega, setFechaEntrega] = useState('')
   const [cliente, setCliente] = useState('')
+
+  const { mostrarAlerta, alerta, submitProyecto } = useProyectos()
+
+  const handleSubmit = async e => {
+    e.preventDefault()
+
+    if([nombre, descripcion, fechaEntrega, cliente].includes('')){
+      mostrarAlerta({
+        msg: 'Todos los campos son obligatorios',
+        error: true
+      })
+
+      return;
+    }
+
+    // Pasa datos al provider
+    await submitProyecto({ nombre, descripcion, fechaEntrega, cliente })
+
+    setNombre('')
+    setDescripcion('')
+    setFechaEntrega('')
+    setCliente('')
+  }
   
   return (
-    <form className='bg-white py-10 px-5 md:w-1/2 rounded-lg shadow'>
+    <form className='bg-white py-10 px-5 md:w-1/2 rounded-lg shadow' onSubmit={handleSubmit}>
+      {alerta.msg && <Alerta alerta={alerta} />}
       <div className="mb-5">
         <label htmlFor="nombre" className='text-gray-700 uppercase font-bold text-sm'>Nombre Proyecto</label>
         <input 
